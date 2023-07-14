4ca100f53290667d63d03f30c48e2e6cfe828771
Alison <alisonaguirre1229@gmail.com>
2023-07-14 09:16:42 -0500

    Initial Commit

---
+++ Project 1/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Project 1
+//
+//  Created by Alison Aguirre on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var fact = ""
+    
+    var body: some View {
+        ZStack {
+            Color(.systemPurple)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("About Me: Alison Aguirre")
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                    
+                Image("me")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                
+                Button("Fun Fact About Me!") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    fact = "I'm a figure skater and my favorite color is purple"
+                    
+                }
+                
+              
+                .font(.title2)
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+                Text(fact)
+                    .font(.title2)
+                    .multilineTextAlignment(.center)
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
